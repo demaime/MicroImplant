2a82782ff6440a64de0b83af6df7482b1435e3b2
demaime <73899078+demaime@users.noreply.github.com>
2021-03-02 20:11:37 -0300

    test language

--- catalogo.js
+++ catalogo.js
@@ -3,7 +3,10 @@ const productos = [
     categoria: "Cabeza",
     nombre: "Chorizo",
     descripcion: "Rico plato de carne y pan",
-    medidas: "90x60x90",
+    medidas: [
+      { titulo: "Largos Poliaxiales", medidas: ["30-45mm", "30-50mm"] },
+      { titulo: "diametro", medidas: ["4.5mm", "5.5mm", "6.5mm", "7.5mm"] },
+    ],
   },
   {
     nombre: "Chorizo 11111",
