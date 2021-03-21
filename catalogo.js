4ef899fac3af3ccc90862a9ebe59b89d58d5daf2
demaime <73899078+demaime@users.noreply.github.com>
2021-03-21 11:24:06 -0300

    nose

--- catalogo.js
+++ catalogo.js
@@ -3,21 +3,30 @@ const productos = [
     categoria: "Columna",
     nombre: "Sistema de Fijación",
     descripcion:
-      "El Sistema de Fijación de raquis Firebird ofrece una amplia solucion para los procesos quirúrgicos toracolumbares tales como procesos degenerativos discales o cirugías mínimamente invasivas.",
+      "El Sistema de Fijación ofrece una amplia solucion para los procesos quirúrgicos toracolumbares tales como procesos degenerativos discales o cirugías mínimamente invasivas.",
     medidas: [
       { titulo: "Largos Poliaxiales", medidas: ["30-45mm", "30-50mm"] },
-      { titulo: "diametro", medidas: ["4.5mm", "5.5mm", "6.5mm", "7.5mm"] },
+      { titulo: "Diámetro", medidas: ["4.5mm", "5.5mm", "6.5mm", "7.5mm"] },
     ],
     imagenes: ["culito.png", "tuvieja.jpeg", "lastfoto.png"],
   },
   {
-    nombre: "Chorizo 11111",
-    descripcion: "Rico plato de carne y pan",
-    medidas: "90x60x90",
+    categoria: "Columna",
+    nombre: "Sistema Interespinoso - Titanio",
+    descripcion:
+      "XSPINOUS es un interespinoso que se utiliza en el tratamiento de la enfermedad degenerativa discal. Presenta un mecanismo de implantación por la vía lateral del espacio interespinoso preservando el ligamento supraespinoso.",
+    medidas: { titulo: "Medidas", medidas: ["8mm", "10mm", "12mm", "14mm"] },
+    imagenes: ["culito.png", "tuvieja.jpeg", "lastfoto.png"],
   },
   {
-    nombre: "Chorizo 33333333333",
-    descripcion: "Rico plato de carne y pan",
-    medidas: "90x60x90",
+    categoria: "Columna",
+    nombre: "Sistema de Fijación",
+    descripcion:
+      "El Sistema de Fijación de raquis Firebird ofrece una amplia solucion para los procesos quirúrgicos toracolumbares tales como procesos degenerativos discales o cirugías mínimamente invasivas.",
+    medidas: [
+      { titulo: "Largos Poliaxiales", medidas: ["30-45mm", "30-50mm"] },
+      { titulo: "Diámetro", medidas: ["4.5mm", "5.5mm", "6.5mm", "7.5mm"] },
+    ],
+    imagenes: ["culito.png", "tuvieja.jpeg", "lastfoto.png"],
   },
 ];
