5f72241ae9ac675d31cd1e47c13db1e1cd6aa556
demaime <73899078+demaime@users.noreply.github.com>
2021-04-08 21:40:35 -0300

    ajustes en containers

--- catalogo.js
+++ catalogo.js
@@ -1,23 +1,27 @@
 const productos = [
   {
-    id: "sistema_fijacion",
+    id: "fijacion_titanio",
     categoria: "Columna",
     nombre: "Sistema de Fijación",
     descripcion:
       "El Sistema de Fijación ofrece una amplia solucion para los procesos quirúrgicos toracolumbares tales como procesos degenerativos discales o cirugías mínimamente invasivas.",
     medidas: [
-      { titulo: "Largos Poliaxiales", medidas: ["30-45mm", "30-50mm"] },
-      { titulo: "Diámetro", medidas: ["4.5mm", "5.5mm", "6.5mm", "7.5mm"] },
+      { titulo: "Largos Poliaxiales", tamaño: ["30-45 mm", "30-50 mm"] },
+      { titulo: "Diámetro", tamaño: ["4.5 mm", "5.5 mm", "6.5 mm", "7.5 mm"] },
     ],
-    imagenes: ["culito.png", "tuvieja.jpeg", "lastfoto.png"],
+    imagenes: [
+      "fijacion_titanio1.jpeg",
+      "fijacion_titanio2.jpg",
+      "fijacion_titanio3.jpg",
+    ],
   },
   {
     id: "interespinoso_titanio",
     categoria: "Columna",
     nombre: "Sistema Interespinoso - Titanio",
     descripcion:
-      "XSPINOUS es un interespinoso que se utiliza en el tratamiento de la enfermedad degenerativa discal. Presenta un mecanismo de implantación por la vía lateral del espacio interespinoso preservando el ligamento supraespinoso.",
-    medidas: { titulo: "Medidas", medidas: ["8mm", "10mm", "12mm", "14mm"] },
+      "Se utiliza en el tratamiento de la enfermedad degenerativa discal. Presenta un mecanismo de implantación por la vía lateral del espacio interespinoso preservando el ligamento supraespinoso.",
+    medidas: { titulo: "Medidas", tamaño: ["8 mm", "10 mm", "12 mm", "14 mm"] },
     imagenes: ["culito.png", "tuvieja.jpeg", "lastfoto.png"],
   },
   {
