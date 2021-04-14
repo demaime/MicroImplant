97ad6f4b2b8c9b497d2ae53c8d88cc6aa652f617
demaime <73899078+demaime@users.noreply.github.com>
2021-04-14 20:18:44 -0300

    ahora anda todo

--- productos/productos-helper.js
+++ productos/productos-helper.js
@@ -1,23 +1,99 @@
-function actualizarMedidas (elemento, arrMedidas) {
-  elemento.innerHTML = arrMedidas.map(medida => {
-    const items = medida.tamano.map(tamano => `<li>${tamano}</li>`).join('')
-    return `
+function actualizarMedidas(elemento, arrMedidas) {
+  elemento.innerHTML = arrMedidas
+    .map((medida) => {
+      const items = medida.tamano
+        .map((tamano) => `<li>${tamano}</li>`)
+        .join("");
+      return `
       <div class="mb-4">
         <p>${medida.titulo}</p>
         <ul class="ml-2">${items}</ul>
       </div>
-    `
-  }).join('')
+    `;
+    })
+    .join("");
 }
 
-function menuColumna(containerProducto, contenedorLista) {
-  containerProducto.classList.toggle("opened");
-  contenedorLista.style.maxHeight = contenedorProductoActual.classList.contains(
-    "opened"
-  )
+function menuColumna(contenedorLista) {
+  contenedorLista.classList.toggle("opened");
+  contenedorLista.style.maxHeight = contenedorLista.classList.contains("opened")
     ? "50rem"
     : 0;
-  contenedorLista.style.zIndex = contenedorProductoActual.classList.contains("opened")
+  contenedorLista.style.zIndex = contenedorLista.classList.contains("opened")
     ? "100"
     : 0;
+}
+
+function selectItem(
+  elemento,
+  nombreElem,
+  descripcionElem,
+  imagenElem,
+  contenedorProductoActual,
+  listaColumna,
+  medidasContainer
+) {
+  const productId = elemento.data;
+  const productClicked = productos.find((product) => product.id === productId);
+  nombreElem.innerText = productClicked.nombre;
+  descripcionElem.innerText = productClicked.descripcion;
+
+  actualizarMedidas(medidasContainer, productClicked.medidas);
+
+  let imgHtml = "";
+  const classesByIndex = ["uno", "dos", "tres"];
+  productClicked.imagenes.forEach((imagen, i) => {
+    imgHtml += `
+      <div 
+        class="${classesByIndex[i]} opacity-${
+      i === 0 ? "1" : "0"
+    } w-full h-96 justify-center items-center flex absolute p-2"
+      >
+        <img
+          class="max-w-full max-h-full"
+          src="../assets/${imagen}"
+          alt=""
+        />
+      </div>
+    `;
+  });
+
+  imagenElem.innerHTML = imgHtml;
+  menuColumna(listaColumna);
+}
+
+function showImage(elem, butSelector) {
+  const imagenArr = Array.from(document.querySelectorAll("#img-container div"));
+  butSelector.forEach((butImg) => {
+    if (elem.dataset.image === butImg.dataset.image) {
+      butImg.classList.add("bg-primary-dark");
+      butImg.classList.remove("bg-gray-400");
+    } else {
+      butImg.classList.remove("bg-primary-dark");
+      butImg.classList.add("bg-gray-400");
+    }
+  });
+  imagenArr.forEach((foto) => {
+    if (foto.classList.contains(elem.dataset.image)) {
+      foto.classList.add("opacity-1");
+      foto.classList.remove("opacity-0");
+    } else {
+      foto.classList.add("opacity-0");
+      foto.classList.remove("opacity-1");
+    }
+  });
+}
+
+function closeMenu(menu, productoActual) {
+  document.addEventListener("click", function (e) {
+    if (
+      menu.classList.contains("opened") &&
+      !menu.contains(e.target) &&
+      productoActual !== e.target
+    ) {
+      menu.classList.remove("opened");
+      menu.style.maxHeight = 0;
+      menu.style.zIndex = 0;
+    }
+  });
 }
